33e3a4b0fbd096fc78d4fa29b573a55b21709004
huveyda.bilgen <huveyda.bilgen@pru.edu.tr>
2020-11-07 19:12:54 +0300

    Add project files.

---
+++ Dxdashboard/frmDesigner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dxdashboard
+{
+    public partial class frmDesigner : Form
+    {
+        public frmDesigner()
+        {
+            InitializeComponent();
+        }
+
+        private void fileNewBarItem1_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+
+        }
+
+        private void fileOpenBarItem1_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+
+        }
+    }
+}
